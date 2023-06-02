39ec4e98696b79191d8752b99d1047cc2ee246f3
JingHan <63907091+henry111383@users.noreply.github.com>
2023-06-02 14:13:55 +0800

    Initial Commit

---
+++ iOS_2023final/iOS_2023finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_2023finalApp.swift
+//  iOS_2023final
+//
+//  Created by Jing Han on 2023/6/2.
+//
+
+import SwiftUI
+
+@main
+struct iOS_2023finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
